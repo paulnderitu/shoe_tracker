13ae9c33c9c018a3898b1e76c219d3f31b1ab6e2
Your Name <youremail@domain.com>
2017-07-20 11:09:12 +0300

    added spec for validaing the availability of a brand of shoes

--- lib/brand.rb
+++ lib/brand.rb
@@ -1,3 +1,3 @@
-class Brand <ActiveRecord::Base 
-
+class Brand < ActiveRecord::Base
+  has_and_belongs_to_many(:distributors)
 end
--- spec/brand_spec.rb
+++ spec/brand_spec.rb
@@ -1,5 +1,9 @@
 require("spec_helper")
 
 describe(Brand) do
+  it("validates the availability of a new brand name") do
+    brand = Brand.new({:name => ""})
+     expect(brand.save()).to(eq(false))
+  end
 
 end
