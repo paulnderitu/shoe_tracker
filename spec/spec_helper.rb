5bf5ca15655dfdb0b75573f76582da9d33652a7b
Your Name <youremail@domain.com>
2017-07-20 10:47:58 +0300

    added spec helper code requiring all necessary files to load my test database, bundle all my gems, required app, both brand and distributor and also configured my database to destory an item from the necesary tables

---
+++ spec/spec_helper.rb
@@ -0,0 +1,24 @@
+
+ENV['RACK_ENV'] = 'test'
+
+require('bundler/setup')
+Bundler.require(:default, :test)
+set(:root, Dir.pwd())
+
+Dir[File.dirname(__FILE__) + '/../lib/*.rb'].each { |file| require file }
+
+require('capybara/rspec')
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+require('./app')
+
+RSpec.configure do |config|
+  config.after(:each) do
+    Distributor.all().each() do |store|
+      store.destroy()
+    end
+    Brand.all().each() do |brand|
+      brand.destroy()
+    end
+  end
+end
