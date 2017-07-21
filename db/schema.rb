5b9b231afde564751c22a8c25baf3aacb1018c67
Your Name <youremail@domain.com>
2017-07-21 17:33:44 +0300

    generated table for brands

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170720094519) do
+ActiveRecord::Schema.define(version: 20170721142143) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "brands", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "distributions", force: :cascade do |t|
     t.string "distributor_name"
